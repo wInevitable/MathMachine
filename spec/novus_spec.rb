cf9d643ede83a88fedf27d69f5b52646b89ea67f
William Noll <williampnoll@gmail.com>
2015-06-09 20:45:23 -0400

    Initial Tests and Ruby Class for Math Machine

---
+++ spec/novus_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require 'novus'
+
+describe MathMachine do
+  subject(:math_machine) { MathMachine.new() }
+
+  it 'adds two numbers together' do
+
+  end
+
+  it 'subtracts one number from another' do
+
+  end
+
+  it 'multiplies two numbers' do
+
+  end
+
+  it 'divides two numbers' do
+
+  end
+
+  it 'rounds properly when dividing' do
+
+  end
+
+  it 'observes order of operations' do
+
+  end
+end
